fd79ba7c73b35312711d057cca7c0f3b25a83f94
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-18 15:46:44 +0100

    inserita ul e template nel documento e inserimento degli elementi in array nella ul

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert()
+/*  creare una todo list che permatta di aggiungere (tramite campo input) e rimuovere gli elementi!
+ */
+
+ // 1. inizializzo array lista della spesa
+var listaSpesa = [
+  "Comprare il latte",
+  "Comprare la pasta",
+  "Comprare i pomodori",
+  "Comprare i salumi"
+];
+
+// 2. clono il template per aggiungere le altre voci alla lista
+for ( var i = 0; i < listaSpesa.length; i++){
+  var template = $(".template li").clone();
+  // aggiungo i vari elementi della lista nei li
+  template.prepend(listaSpesa[i]);
+  $(".todo-list").append(template);
+}
